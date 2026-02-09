a0a9fdb97c41785e456fdafc33aa512fc32af818
msVill <msv.ltmc@gmail.com>
2026-02-08 20:56:59 -0800

    Added - fixed error with output function return type

--- 210-lab-4A.cpp
+++ 210-lab-4A.cpp
@@ -1,8 +1,7 @@
 // COMSC 210 | Martha Stephanie Villalta | Lab 4A
 // Prog. creates a vector of structs [Color].
 // Red, Blue, and Green variables will rep. the hues.
-// I think that each var. will receive random numbers
-// From the table: 4 rows: Color (1 - 13)(output the vector's contents)
+
 #include <random>
 #include <iostream>
 #include <vector>
@@ -13,8 +12,8 @@ struct Color {
     int red, blue, green;
 };
 
-int inputColorData(); // prototype.
-void outputColorData(const ); // prototype.
+Color inputColorData(); // prototype.
+void outputColorData(const Color& c); // prototype.
 
 int main() {
 
@@ -23,6 +22,8 @@ int main() {
     return 0;
 }
 
-int intputColorData(){ // function needs to handle data input to variable/object.
-    Color colorTemp = 
-};
+Color inputColorData(){ // function needs to handle data input to variable/object.
+    Color colorTemp;
+
+
+}
