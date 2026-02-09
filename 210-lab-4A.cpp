614268cdb24c08d3a51bcb37fb7e74f78c6c0f36
msVill <msv.ltmc@gmail.com>
2026-02-08 20:39:02 -0800

    Added function prototypes

--- 210-lab-4A.cpp
+++ 210-lab-4A.cpp
@@ -1,6 +1,17 @@
+// COMSC 210 | Martha Stephanie Villalta | Lab 4A
+#include <random>
+#include <iostream>
 using namespace std;
 
-int main(){
+struct Color {
+    int red, blue, green;
+};
+
+int inputColorData(); // prototype.
+
+int main() {
+
+    vector<Color> colors;
 
     return 0;
 }
